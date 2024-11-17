00ef50794b37517bf1643e06752f499d315fa37d
irinadeeva <deeva97@me.com>
2024-11-17 19:41:07 +0300

    Initial Commit

---
+++ Task 6 Inertial Square/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 6 Inertial Square
+//
+//  Created by Irina Deeva on 17/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
